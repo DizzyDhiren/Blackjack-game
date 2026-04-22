c7e0a6190b0cbef01a8de1f47bd5ffa63b4012a5
DizzyDhiren <dhiren007patel@gmail.com>
2026-04-22 02:22:41 +0100

    fix: changed if statement to <= 20

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let firstCard = 10;
-let secondCard = 12;
+let secondCard = 11;
 
 let sum = firstCard + secondCard;
 
 
-if (sum < 21){
+if (sum <= 20){
     console.log("Do you want to draw another card?");
 } else if (sum === 21){
     console.log("You've got a blackjack!");
